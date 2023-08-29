8705f3a5416edf7d9afa9412c96dd662811a8d16
PRITAM CHAKROBORTY <92992609+anynomous001@users.noreply.github.com>
2023-08-30 00:41:34 +0530

    Add files via upload
    completed first page of the form

--- src/Routes/Secondpage.js
+++ src/Routes/Secondpage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import Footer from '../components/Footer'
+import Secondform from '../components/Secondform'
 const Secondpage = () => {
     return (
         < ><Navbar />
-            <p>Secondpage</p></>
+            <Secondform />
+            <Footer />
+        </>
     )
 }
 
---
+++ src/components/Secondform.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Secondform.css'
+
+const Secondform = () => {
+    return (
+        <div className='outer-div'>
+            <div className='form-container'>
+                <h1>Select Your Plan</h1>
+                <h4>You have the option of monthly or yearly billing.</h4>
+                <form>
+
+
+                </form>
+
+            </div>
+        </div>)
+}
+
+export default Secondform
